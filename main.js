994f615de8e5251f5c46627d467b476d3439cf61
chue wathan kyaw <chuewathan.k24@gmail.com>
2022-04-04 06:57:16 +0630

    origin

---
+++ main.js
@@ -0,0 +1,24 @@
+// execute scriot after page load
+window.onload = function digital_fn(){
+
+    // toggle button
+    let toggle = document.querySelector("#nav .toggle-btn");
+let collapse = document.querySelector("#nav .collapse");
+
+toggle.addEventListener('click',function(event){
+    collapse.classList.toggle('active')
+    // console.log(toggle)
+});
+
+
+// mansonty js
+
+let grid = document.querySelector("#site-main .recent-work-area .images-flex");
+
+let msnry = new Masonry(grid,{
+    itemSelector:'.flex-item',
+    gutter:100,
+    fitWidth:true
+})
+
+}
